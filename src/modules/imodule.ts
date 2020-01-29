acbe04c58582e27540714300bdc8ef0923b916ab
Benjamin Caspari <caspari@rossmanith.com>
2020-01-29 16:08:49 +0100

    module ids

--- src/modules/imodule.ts
+++ src/modules/imodule.ts
@@ -1,6 +1,9 @@
+// Must match the values defined in Enums_Module.cs in Roxtra.DataProvider
 export enum ModuleId {
+  None = 0,
   EForm = 1,
-  RiskManagement = 2
+  RiskManagement = 2,
+  DocumentManagement = 3
 }
 
 export interface IModule {
