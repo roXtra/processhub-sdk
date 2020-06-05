d67e4274cc35237d55716ae9b0b521c336ce4a3a
Claudius Eisele <eisele@roxtra.com>
2020-06-05 10:00:32 +0200

    Enable RenovateBot to automatically create dependency update PRs

---
+++ renovate.config.js
@@ -0,0 +1,33 @@
+const branchName = 'auto-dep-update';
+
+module.exports = {
+  branchPrefix: `${branchName}/`,
+  enabledManagers: ['github-actions', 'regex', 'npm'],
+  gitAuthor: 'Dependency Bot <devbot@roxtra.com>',
+  logLevel: 'info',
+  onboarding: true,
+  onboardingBranch: `${branchName}/configure`,
+  platform: 'github',
+  regexManagers: [
+    {
+      datasourceTemplate: 'github-tags',
+      fileMatch: ['^\\.github/workflows/[^/]+\\.ya?ml$'],
+      matchStrings: ['uses: (?<depName>.*?)@(?<currentValue>.*?)\\s'],
+    },
+  ],
+  repositories: [
+    'roXtra/processhub-sdk',
+  ],
+  rebaseWhen: "behind-base-branch",
+  ignoreDeps: [],
+  packageRules: [
+    {
+      packageNames: ["bpmn-moddle"],
+      allowedVersions: "~6.0.6"
+    },
+    {
+      packageNames: ["@types/node"],
+      allowedVersions: "^12.0.0"
+    }
+  ]
+};
