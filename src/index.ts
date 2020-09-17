212319b26b7c7161d81ff5a8a434490321bb88d3
Johannes Preßmar <pressmar@rossmanith.com>
2020-09-17 11:42:31 +0200

    Remove roxtraapi and hasMailStartEvent

--- src/index.ts
+++ src/index.ts
@@ -46,5 +46,3 @@ import * as ServerConfig from "./serverconfig";
 export { ServerConfig };
 import * as Modules from "./modules";
 export { Modules };
-import * as RoxtraAPI from "./roxtraapi";
-export { RoxtraAPI };
--- src/roxtraapi/index.ts
+++
@@ -1,5 +0,0 @@
-export * from "./apitypes";
-export * from "./crmapi";
-export * from "./svnapi";
-export * from "./mailapi";
-export * from "./apiutils";
