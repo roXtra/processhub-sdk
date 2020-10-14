8059da8db782aeecd5a40e61f48641d9f28b89a4
Sebastian Schürle <schuerle@roxtra.com>
2020-10-14 11:46:38 +0200

    Changed API Signature for Templates
    EF-1309

--- src/process/legacyapi.ts
+++ src/process/legacyapi.ts
@@ -41,7 +41,7 @@ export interface IProcessReply extends IBaseMessage {
 }
 
 export interface ITemplateProcessesReply extends IBaseReply {
-  templates: { name: string; xml: string; metaData: { shortDescription: string; description: string } }[];
+  templates: { config: string; xml: string; metaData: { shortDescription: string; description: string } }[];
 }
 
 export interface ICreateProcessRequest extends IBaseRequest {
