23145971ef6d5358cfdf4061972d742089247fc1
BCaspari <caspari@rossmanith.com>
2021-02-02 16:21:18 +0100

    * BpmnError inherits from Error (as custom Errors should) * added type guard for BpmnError (instanceof does not work if a Service uses another SDK version as the main program)

--- src/instance/bpmnerror.ts
+++ src/instance/bpmnerror.ts
@@ -4,14 +4,19 @@ export enum ErrorCode {
   ConfigInvalid = "CONFIG_INVALID",
 }
 
-export class BpmnError {
+export class BpmnError extends Error {
   public readonly errorCode: ErrorCode | string;
   public readonly errorMessage: string;
   public readonly innerError: Error | undefined;
 
   public constructor(errorCode: string, errorMessage: string, innerError?: Error) {
+    super(errorMessage);
     this.errorCode = errorCode;
     this.errorMessage = errorMessage;
     this.innerError = innerError;
   }
+}
+
+export function isBpmnError(error: {}): error is BpmnError {
+  return error && error.constructor.name === "BpmnError" && typeof (error as BpmnError).errorMessage === "string" && typeof (error as BpmnError).errorCode === "string";
 }
