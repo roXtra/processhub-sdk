11c4706b8da7916389c41491bff7b7866a9dd794
Johannes Preßmar <pressmar@roxtra.com>
2021-02-24 11:40:18 +0100

    Remove obsolete linter line exceptions in array.ts

--- src/tools/array.ts
+++ src/tools/array.ts
@@ -3,12 +3,10 @@ interface Array<T> {
   last(): T;
 }
 
-// eslint-disable-next-line @typescript-eslint/unbound-method
 Array.prototype.last = function <T>(): T | undefined {
   if (this) {
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
     if (this.length > 0) {
-      // eslint-disable-next-line @typescript-eslint/no-unsafe-return,@typescript-eslint/no-unsafe-member-access
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
       return this[this.length - 1];
     } else {
       return undefined;
