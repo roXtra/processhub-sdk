2d3c431668d27d223a2c0accd1fa138fb69cfd6d
Johannes Preßmar <pressmar@roxtra.com>
2021-05-12 09:03:58 +0200

    Add test for process validation

--- src/process/processvalidation.test.ts
+++ src/process/processvalidation.test.ts
@@ -6,46 +6,46 @@ import { createId } from "../tools/guid";
 describe("sdk", function () {
   describe("process", function () {
     describe("validation", function () {
-      it("soll Process-Details Validierung testen", function () {
-        let obj = { displayName: "test1234", description: "hallo test" };
+      it("should test displayName and description of process details validation", function () {
+        let obj: Partial<IProcessDetails> = { displayName: "test1234", description: "hallo test" };
         let res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isTrue(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isTrue(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = { displayName: "test12", description: "hallo test" };
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isTrue(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isTrue(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = { displayName: "te", description: "hallo test" };
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isFalse(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isFalse(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = { displayName: "test 1234", description: "hallo test" };
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isTrue(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isTrue(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = { displayName: "", description: "hallo test" };
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isTrue(!res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isTrue(!res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = { displayName: "123456789012345678901234567890123456789012345678901", description: "hallo test" };
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isFalse(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isFalse(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = { displayName: "12345678901234567890123456789012345678901234567890", description: "hallo test" };
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isTrue(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isTrue(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = { displayName: "test1234", description: "" };
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isTrue(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isTrue(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = { displayName: "test1234", description: "" };
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isTrue(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isTrue(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = { displayName: "012345678901234567890123456789012345678901234567891", description: "hallo test" };
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isFalse(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isFalse(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
 
         obj = {
           workspaceId: createId(),
@@ -57,7 +57,57 @@ describe("sdk", function () {
         } as IProcessDetails;
 
         res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
-        assert.isTrue(res, "error: " + obj.displayName + " - " + obj.description);
+        assert.isTrue(res, "error: " + String(obj.displayName) + " - " + String(obj.description));
+      });
+
+      it("should test deletionPeriod and retentionPeriod of process details validation", function () {
+        let obj: Partial<IProcessDetails> = { displayName: "test", description: "test", deletionPeriod: 0, retentionPeriod: 0 };
+        let res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isTrue(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: 0, retentionPeriod: 1 };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isFalse(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: 1, retentionPeriod: 0 };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isTrue(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: 1, retentionPeriod: 1 };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isTrue(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: 1, retentionPeriod: 2 };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isFalse(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: 2, retentionPeriod: 1 };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isTrue(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: undefined, retentionPeriod: 1 };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isTrue(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: 1, retentionPeriod: undefined };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isTrue(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: undefined, retentionPeriod: undefined };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isTrue(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: undefined, retentionPeriod: -1 };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isFalse(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: -1, retentionPeriod: undefined };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isFalse(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
+
+        obj = { ...obj, deletionPeriod: -1, retentionPeriod: -1 };
+        res = ProcessValidation.isProcessDetailsValid(obj as IProcessDetails);
+        assert.isFalse(res, "error: " + String(obj.deletionPeriod) + " - " + String(obj.retentionPeriod));
       });
     });
   });
