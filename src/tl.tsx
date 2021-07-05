48b0e38ced221792d046023358d25a1f6f99465a
BCaspari <caspari@rossmanith.com>
2021-07-05 16:28:35 +0200

    tl and TL functions take language and module id for translations

--- src/tl.tsx
+++ src/tl.tsx
@@ -1,13 +1,45 @@
+import { ModuleId } from "./modules";
+
 export interface ITLProps {
   text: string;
+  language?: string;
+  moduleId?: number;
 }
 
 // eslint-disable-next-line @typescript-eslint/naming-convention
 export function TL(props: ITLProps): JSX.Element {
-  return <span>{props.text}</span>;
+  return <span>{tl(props.text, props.language, props.moduleId)}</span>;
 }
 
+const translations: { [language: string]: ITranslations | undefined } = {};
+
 // Variante für Strings, die nicht im JSX eingefügt werden
-export function tl(text: string): string {
+export function tl(text: string, language?: string, moduleId?: number): string {
+  if (language) {
+    const translationsForLanguage = translations[language];
+    if (translationsForLanguage) {
+      const id = moduleId || ModuleId.None;
+      const translationsForModule = translationsForLanguage.translations.find((t) => t.moduleIds.includes(id) && t.strings[text] != null);
+      if (translationsForModule && translationsForModule.strings[text]) {
+        return translationsForModule.strings[text];
+      }
+      const translationsForDefaultModule = translationsForLanguage.translations.find((t) => t.moduleIds.includes(ModuleId.None));
+      if (translationsForDefaultModule && translationsForDefaultModule.strings[text]) {
+        return translationsForDefaultModule.strings[text];
+      }
+    }
+  }
   return text;
+}
+
+export function registerTranslations(newTranslations: ITranslations): void {
+  translations[newTranslations.language] = newTranslations;
+}
+
+export interface ITranslations {
+  language: string;
+  translations: {
+    moduleIds: number[];
+    strings: { [source: string]: string };
+  }[];
 }
