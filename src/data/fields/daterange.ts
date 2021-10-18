5a11fa9ea94e2b59e1bce8f3af756fc512412bef
Johannes Preßmar <pressmar@roxtra.com>
2021-10-18 15:21:39 +0200

    Add ProcessHubDateRange field

---
+++ src/data/fields/daterange.ts
@@ -0,0 +1,23 @@
+import Joi from "joi";
+import { IFieldConfigDefault, IFieldConfigObject } from "../datainterfaces";
+
+export interface IDateRangeFieldValue {
+  start: Date;
+  end: Date;
+}
+
+const IDateRangeFieldValueObject: IDateRangeFieldValue = {
+  start: Joi.date().required() as unknown as Date,
+  end: Joi.date().required() as unknown as Date,
+};
+
+export const IDateRangeFieldValueSchema = Joi.object(IDateRangeFieldValueObject);
+
+export type IDateRangeFieldConfig = IFieldConfigDefault<IDateRangeFieldValue>;
+
+const IDateRangeFieldConfigObject: IDateRangeFieldConfig = {
+  defaultValue: IDateRangeFieldValueSchema as unknown as IDateRangeFieldValue,
+  ...IFieldConfigObject,
+};
+
+export const IDateRangeFieldConfigSchema = Joi.object(IDateRangeFieldConfigObject);
