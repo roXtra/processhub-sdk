8bb5a10e82e8042f38c7838e7702cec5a19cab22
schuerle@roxtra.com <schuerle@roxtra.com>
2021-11-03 09:18:40 +0100

    Changed naming of options in IDropdownFieldConfig
    Ef-2046

--- src/data/fields/dropdown.ts
+++ src/data/fields/dropdown.ts
@@ -2,12 +2,12 @@ import Joi from "joi";
 import { IFieldConfigDefault, IFieldConfigObject } from "../datainterfaces";
 
 export interface IDropdownFieldConfig extends IFieldConfigDefault<string> {
-  options: string[];
+  entries: string[];
 }
 
 const IDropdownFieldConfigObject: IDropdownFieldConfig = {
   defaultValue: Joi.string().allow("") as unknown as string,
-  options: Joi.array().items(Joi.string().allow("")).required() as unknown as string[],
+  entries: Joi.array().items(Joi.string().allow("")).required() as unknown as string[],
   ...IFieldConfigObject,
 };
 
