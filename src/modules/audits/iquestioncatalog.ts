85014d7ba27678f0ba435c88f79654ecad7d2179
Johannes Preßmar <pressmar@roxtra.com>
2022-03-10 10:52:55 +0100

    Create inital question catalog interface EF-2206

---
+++ src/modules/audits/iquestioncatalog.ts
@@ -0,0 +1,28 @@
+import { IUserDetailsSmall } from "../../user/userinterfaces";
+
+export interface IQuestion {
+  id: string;
+  noteText: string;
+  questionText: string;
+}
+
+export interface IQuestionCatalogNode {
+  id: string;
+  text: string;
+  questions: IQuestion[];
+  childNodes: IQuestionCatalogNode[];
+}
+
+export interface IQuestionCatalogRevision {
+  counter: number;
+  editedBy: IUserDetailsSmall;
+  editDate: Date;
+  hasNodesChanged: boolean;
+  hasQuestionsChanged: boolean;
+}
+
+export default interface IQuestionCatalog {
+  id: string;
+  revision: IQuestionCatalogRevision;
+  nodes: IQuestionCatalogNode[];
+}
