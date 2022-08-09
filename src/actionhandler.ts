2ea7259b009848975212ed9388f30f8d5e29bab3
Johannes Preßmar <pressmar@roxtra.com>
2022-08-09 10:36:29 +0200

    Remove environment as parameter from requestExtras EF-2940, EF-2937

--- src/actionhandler.ts
+++ src/actionhandler.ts
@@ -2,7 +2,6 @@ import { WorkspaceExtras } from "./workspace/workspaceinterfaces";
 import { ProcessExtras } from "./process/processinterfaces";
 import { InstanceExtras } from "./instance/instanceinterfaces";
 import { UserExtras } from "./user/userinterfaces";
-import { ICoreEnvironment } from "./environment";
 
 export interface IExtrasRequest {
   workspaceExtras?: WorkspaceExtras;
@@ -14,7 +13,7 @@ export interface IExtrasRequest {
 export abstract class ActionHandler {
   // Load Page "/@workspace/..."
   public abstract gotoPage(path: string): void;
-  public abstract requestExtras(environment: ICoreEnvironment, requestedExtras: IExtrasRequest, forceReload?: boolean): Promise<void>;
+  public abstract requestExtras(requestedExtras: IExtrasRequest, forceReload?: boolean): Promise<void>;
   public abstract openInstancePopup(workspaceId: string, instanceId: string, todoId: string | undefined, readonly: boolean): Promise<void>;
   public abstract openTodoPopup(workspaceId: string, instanceId: string, todoId: string): Promise<void>;
 }
