7f334eeeed963f97d64d1e42eae25df90c358d00
Johannes Preßmar <pressmar@roxtra.com>
2022-08-25 13:53:25 +0200

    Add ICustomRating EF-2658

--- src/modules/audits/iauditssettings.ts
+++ src/modules/audits/iauditssettings.ts
@@ -1,5 +1,7 @@
+import { ICustomRating } from "./icustomrating";
 import IQuestionCatalog from "./iquestioncatalog";
 
 export default interface IAuditsSettings {
   questionCatalog: IQuestionCatalog;
+  customRating: ICustomRating;
 }
