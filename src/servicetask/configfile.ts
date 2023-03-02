e0e161245f6b74661fe1648a825f7a342c669260
Benjamin Caspari <caspari@rossmanith.com>
2023-03-02 13:58:03 +0100

    added function to read config.json of a service EF-3445

---
+++ src/servicetask/configfile.ts
@@ -0,0 +1,43 @@
+import Joi from "joi";
+import fs from "fs/promises";
+import { validateType } from "../data/datatools";
+import { IServiceTaskLogger } from "./servicetaskenvironment";
+import { BpmnError, ErrorCode } from "../instance/bpmnerror";
+
+export interface IServiceConfigSecret {
+  secret: { [key: string]: string };
+}
+
+const IServiceConfigObject: IServiceConfigSecret = {
+  secret: Joi.object().pattern(Joi.string(), Joi.string()) as unknown as { [key: string]: string },
+};
+
+export const IServiceConfigSchema = Joi.object(IServiceConfigObject);
+
+/**
+ * Reads the config file of a service
+ * @param configPath path to config.json
+ * @param schema Joi schema to validate config file
+ * @param logger logger from service task environment
+ * @returns the config from the file or undefined, if the file was not found. If the data could not be read or joi validation failed, a BpmnError is thrown.
+ */
+export async function readConfigFile<T = IServiceConfigSecret>(
+  configPath: string,
+  schema: Joi.Schema<T> = IServiceConfigSchema,
+  logger: IServiceTaskLogger,
+): Promise<T | undefined> {
+  try {
+    const configData = await fs.readFile(configPath, "utf8");
+    const config = validateType<T>(schema, JSON.parse(configData));
+    return config;
+  } catch (ex) {
+    if ((ex as NodeJS.ErrnoException)?.code === "ENOENT") {
+      // Config file does not exist - use empty secrets
+      logger.info(`Webhook service: Config file ${configPath} does not exist.`);
+      return undefined;
+    } else {
+      logger.error("Webhook service failed to load config file: " + String(ex));
+      throw new BpmnError(ErrorCode.ConfigInvalid, "Could not load config file " + configPath, ex instanceof Error ? ex : undefined);
+    }
+  }
+}
