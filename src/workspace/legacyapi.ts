9d8f8a0df013ff5f45e21e8b4866812d401384a0
lwolf-roxtra <leon.wolf@roxtra.com>
2023-12-07 12:47:04 +0100

    Added interfaces for implementation of pdf export feature (workspace report).

--- src/workspace/legacyapi.ts
+++ src/workspace/legacyapi.ts
@@ -1,6 +1,9 @@
-import { IBaseRequest, IBaseReply } from "../legacyapi/apiinterfaces";
+import Joi from "joi";
+import { IGenerateReportRequestType } from "../instance/legacyapi";
+import { IBaseRequest, IBaseReply, IBaseRequestObject } from "../legacyapi/apiinterfaces";
 import { WorkspaceExtras, IWorkspaceDetails } from "./workspaceinterfaces";
 import { IWorkspaceRoles } from "./workspacerights";
+import { createLiteralTypeRegExp } from "../data/regextools";
 
 // WorkspaceRequestRoutes
 export const WorkspaceRequestRoutes = {
@@ -33,3 +36,50 @@ export enum RequestedWorkspaceReportType {
    */
   AUDIT_TRAIL = 2,
 }
+
+const IGenerateReportRequestTypeOptions = ["docx", "pdf"] as const;
+
+/**
+ * Represents common data for Request Report request for workspace
+ */
+export interface IGenerateReportForWorkspaceCommonData<TYPE extends RequestedWorkspaceReportType> extends IBaseRequest {
+  reportType: TYPE;
+  workspaceId: string;
+  draftId: string;
+  resultingFileType: IGenerateReportRequestType;
+}
+
+/**
+ * Represents a Generate Report request for the regular workspace
+ */
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface IGenerateReportForWorkspaceRequest extends IGenerateReportForWorkspaceCommonData<RequestedWorkspaceReportType.WORKSPACE_REGULAR> {
+  // Requires no additional data
+}
+
+/**
+ * Represents a Generate Report request for the workspace audit trail
+*/
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+export interface IGenerateReportForWorkspaceAuditTrailRequest extends IGenerateReportForWorkspaceCommonData<RequestedWorkspaceReportType.AUDIT_TRAIL> {
+  // Requires no additional data
+}
+
+/**
+ * Helper union type for RequestedInstanceReportType
+ */
+type RequestedWorkspaceReportUnionType = RequestedWorkspaceReportType.WORKSPACE_REGULAR | RequestedWorkspaceReportType.AUDIT_TRAIL;
+
+export type IGenerateWorkspaceReportRequest = IGenerateReportForWorkspaceRequest | IGenerateReportForWorkspaceAuditTrailRequest;
+
+const IGenerateWorkspaceReportRequestObject: IGenerateWorkspaceReportRequest = {
+  reportType: Joi.number().valid(RequestedWorkspaceReportType.WORKSPACE_REGULAR, RequestedWorkspaceReportType.AUDIT_TRAIL).required() as unknown as RequestedWorkspaceReportUnionType,
+  workspaceId: Joi.string().required() as unknown as string,
+  draftId: Joi.string().required() as unknown as string,
+  resultingFileType: Joi.string().pattern(createLiteralTypeRegExp(Object.values(IGenerateReportRequestTypeOptions))).required() as unknown as IGenerateReportRequestType,
+
+  // Extends IBaseRequest
+  ...IBaseRequestObject,
+};
+
+export const IGenerateReportRequestSchema = Joi.object(IGenerateWorkspaceReportRequestObject);
