bc3c146a8138f9f005ba2d6453a97fe05e4dacfd
Benjamin Caspari <caspari@roxtra.com>
2024-04-19 15:48:21 +0200

    added datatable field EF-4125

---
+++ src/data/fields/datatable.ts
@@ -0,0 +1,18 @@
+import { IFieldConfig } from "../datainterfaces.js";
+
+export interface IDataTableFieldConfig extends IFieldConfig {
+  columns: {
+    id: string;
+    name: string;
+    type: "text" | "numeric" | "date";
+  }[];
+}
+
+export interface IDataTableFieldValue {
+  rows: {
+    selected?: boolean;
+    data: {
+      [field: string]: string | number | Date;
+    };
+  }[];
+}
