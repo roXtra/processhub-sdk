86e051869b2bb8684290413100af4bf60f0c2ac5
BCaspari <caspari@roxtra.com>
2024-10-07 09:48:23 +0200

    set auth token in header from oidc user EF-4455

---
+++ src/legacyapi/auth.ts
@@ -0,0 +1,32 @@
+import { User } from "oidc-client-ts";
+
+export const authConfig = {
+  authority: "/roxtra/auth",
+  clientId: "95C33BCB-54B3-4F6E-B247-3F2DBB0FFAA6",
+};
+
+function getUser() {
+  // Example: oidc.user:https://uidev.roxtra.com/roxtra/auth:roXtra
+  const itemName = `oidc.user:${authConfig.authority}}:${authConfig.clientId}`;
+  const oidcStorage = localStorage.getItem(itemName);
+  if (!oidcStorage) {
+    return undefined;
+  }
+  return User.fromStorageString(oidcStorage);
+}
+
+/**
+ * Returns the user token that should be used to make requests to the backend.
+ * @returns The token that should be used to make requests to the backend
+ */
+export function getUserToken() {
+  if (typeof window !== "undefined") {
+    // We have to use the xAccesstoken from the initial config if it is set, because this is the token set for anonymous start events to override the user token
+    if (window.__INITIAL_CONFIG__.xAccesstoken) {
+      return window.__INITIAL_CONFIG__.xAccesstoken;
+    }
+    return getUser()?.access_token;
+  }
+
+  return undefined;
+}
