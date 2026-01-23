5305ea14018724d8bc0250dcd29e9ce66566fa43
Viktor Fritsch <viktor.fritsch@roxtra.com>
2026-01-23 15:09:48 +0100

    Remove own diagram-js type declarations

--- src/process/types/diagram-js/diagramjsconsts.ts
+++
@@ -1,8 +0,0 @@
-// S. diagram-js/lib/draw/BaseRenderer.js
-export const DEFAULT_RENDER_PRIORITY = 1000;
-
-// S. diagram-js/lib/core/index.js
-export const ELEMENTFACTORY_SERVICENAME = "elementFactory";
-export const ELEMENTREGISTRY_SERVICENAME = "elementRegistry";
-
-export const MIN_LANEHEIGHT = 10.0;
